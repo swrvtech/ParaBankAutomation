78a0aeb41ecf422a84524a6f7e7869add85940b4
SWRV Tech <admin@swrvtech.com>
2024-07-31 10:10:04 -0400

    testing

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features/",
         glue = "steps",
-        dryRun = false,
+        dryRun = true,
         tags = "@updatePersonalInfo",
         plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json",
                 "rerun:target/failed.txt"}
